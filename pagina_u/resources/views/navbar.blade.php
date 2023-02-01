685dbc575fe2adadcbdd0eaaa81ee45c3b29d5d8
TillWurner <85416653+TillWurner@users.noreply.github.com>
2023-02-01 00:28:46 -0400

    navbar

---
+++ pagina_u/resources/views/navbar.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+ 
+        <table width="100%" style="background-color: #004080 ; margin-left: -50px ; margin-top: -12px ; width: 1578px;">
+          <tbody><tr> 
+            <td width="62%"></td>
+            <td width="12%"><div align="right"><img src={{URL::asset('/img/F1.jpg')}} width="102" height="75"></div></td>
+            <td width="10%"><div align="right"><img src={{URL::asset('/img/F2.jpg')}} width="78" height="91"></div></td>
+            <td width="16%"><div align="right"><img src={{URL::asset('/img/F3.jpg')}} width="116" height="81"></div></td>
+          </tr></tbody>
+        </table>   
+</head>
