212ed29ae6e3a7fc6d679ae1426ae8e55cef63c0
RodrigoJaldin <85416473+RodrigoJaldin@users.noreply.github.com>
2023-01-31 23:58:31 -0400

    gasf

--- pagina_u/resources/views/auth/login.blade.php
+++ pagina_u/resources/views/auth/login.blade.php
@@ -1,76 +1,67 @@
-@extends('layouts.app')
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div align="center">
-                        <div class="card-header">{{ __('Inicio de sesión') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Universidad Autónoma Gabriel René Moreno | Consultas de Datos: Estudiantes</title>
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="manifest" href="/img/site.webmanifest">
+    <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#a30d0a">
+</head>
+<body>
+    <div>
+        <p class="col-xl-4 col-lg-5 col-md-7 col-sm-10 col-xs-12 my-5 mx-auto" style="min-width: 200px; max-width: 500px;">
+            <a style="text-decoration: none" href="https://perfil.uagrm.edu.bo">
+                <img style="width: 100%;" src="https://www.uagrm.edu.bo/img/logo-88x707-gray.png" alt="Logo UAGRM">
+            </a>
+        </p>
+    </div>
+    <div class="container mt-4">
+        <div class="card text-center" style="width: 18rem; margin: 0 auto;">
+            <div class="card-header">
+                Inicio de sesión
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Estudiantes</h5>
+                <div id="message"></div>
+                <form >
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-lg" id="username" placeholder="Código">
                     </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="row mb-3">
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-secondary">
-                                        {{ __('Iniciar Sesion') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Olvidaste tu contraseña?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-lg" id="password" placeholder="Contraseña">
                     </div>
-                </div>
+                    @if (auth()->username == 221177973 && auth()->password == 13484862Sc )
+
+                    @endif
+                    <button type="button" class="btn btn-secondary btn-lg btn-block" id="login">Iniciar Sesión</button>
+                </form>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="#" target="_blank">¿Olvidaste tu contraseña?</a></li>
+                </ul>
+                <h6 class="card-subtitle mt-3 mb-2 text-muted">Acceso directo a:</h6>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><a href="#" target="_blank">Inscripción Web</a></li>
+                    <li class="list-group-item"><a href="#" target="_blank">Formulario de seguimiento a Titulados</a></li>
+                </ul>
             </div>
         </div>
     </div>
-@endsection
+
+    <script src="/js/jquery-3.4.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
